b182c857eae8266cc2df899fbcada06aa5cead7a
CodeCasterNL <github@codecaster.nl>
2021-09-10 16:33:44 +0200

    feat: read BUZ_API_URL from environment variables to allow custom endpoints during development, disable SSL verification when set

--- src/ClientFactory.php
+++ src/ClientFactory.php
@@ -23,10 +23,23 @@ class ClientFactory
         string $clientId,
         string $clientSecret,
         string $environment = Client::ENVIRONMENT_LIVE
-    ): Client {
-        $baseUri = $environment === Client::ENVIRONMENT_STAGING ? Client::URL_STAGING : Client::URL_LIVE;
+    ): Client
+    {
+        $baseUriOverride = getenv('BUZ_API_URL');
 
-        $client = new Client(HttpClient::createForBaseUri($baseUri), new Serializer());
+        $httpClientOptions = array();
+
+        if (false !== $baseUriOverride && '' !== $baseUriOverride) {
+            $baseUri = $baseUriOverride;
+            $httpClientOptions['verify_peer'] = false;
+        } else {
+            $baseUri = $environment === Client::ENVIRONMENT_STAGING ? Client::URL_STAGING : Client::URL_LIVE;
+        }
+
+        $serializer = new Serializer();
+        $httpClient = HttpClient::createForBaseUri($baseUri, $httpClientOptions);
+
+        $client = new Client($httpClient, $serializer);
         $client->setCredentials($clientId, $clientSecret);
 
         return $client;
